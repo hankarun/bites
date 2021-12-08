83d59b1e7f95170a604b31a592ac0693a9245898
hankarun <hankarun@gmail.com>
2021-12-09 00:13:18 +0300

    hard question.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char const* argv[]) {
     aliiskenderturan::NumberSearch numberSearch;
-    numberSearch.SetTargetNumber(302);
+    numberSearch.SetTargetNumber(48156);
     numberSearch.SetInputNumbers("2 3 7 10 50 25");
     std::cout << numberSearch.GetSolution() << std::endl;
     return 0;

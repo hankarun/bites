916bd6da53db538d0c1dc8b21d285bdf524cc71a
hankarun <hankarun@gmail.com>
2021-12-08 23:31:21 +0300

    finish1

--- aliiskenderturan.hpp
+++ aliiskenderturan.hpp
@@ -2,136 +2,151 @@
 #include <array>
 #include <sstream>
 #include <stack>
-#include <string>
 #include <vector>
 
-
 namespace aliiskenderturan {
-std::array<char, 4> operators = {'+', '-', '/', '*'};
-int target = 0;
-bool found = false;
-std::string resultString;
+const std::array<char, 4> operators = {'+', '-', '/', '*'};
 
-int caluculate(int a, int b, char op, bool &valid) {
-  valid = true;
-  if (op == '+')
-    return a + b;
-  if (op == '-')
-    return a - b;
-  if (op == '*')
-    return a * b;
-  if (op == '/') {
-    if (b == 0) {
-      valid = false;
-      return 0;
+class PostFixEquation {
+public:
+  void addItem(const char *value) { data.push_back(value); }
+
+  std::string toInFix() {
+    std::stack<std::string> expr;
+    for (auto &t : data) {
+      if (std::find(std::begin(operators), std::end(operators), *t) ==
+          std::end(operators)) {
+        expr.push(t);
+      } else {
+        auto f1 = expr.top();
+        expr.pop();
+        auto f2 = expr.top();
+        expr.pop();
+        expr.push("(" + f1 + t[0] + f2 + ")");
+      }
     }
-    float s = a / (float)b;
-    if (std::fmod(s, 1.0) == 0)
-      return (int)s;
-    valid = false;
-    return -1;
+    if (!expr.empty())
+      return expr.top();
+    return "";
   }
-  return 0;
-}
 
-int calculateFromPostfix(const std::vector<const char *> &postfix) {
-  std::stack<int> expr;
-  for (auto &t : postfix) {
-    if (std::find(std::begin(operators), std::end(operators), *t) ==
-        std::end(operators)) {
-      expr.push(std::atoi(t));
-    } else {
-      auto f1 = expr.top();
-      expr.pop();
-      auto f2 = expr.top();
-      expr.pop();
-      bool operationValid = false;
-      int result = caluculate(f1, f2, t[0], operationValid);
-      if (!operationValid)
+  bool calculateResult(int &result) {
+    std::stack<int> expr;
+    for (auto &t : data) {
+      if (std::find(std::begin(operators), std::end(operators), *t) ==
+          std::end(operators)) {
+        expr.push(std::atoi(t));
+      } else {
+        auto f1 = expr.top();
+        expr.pop();
+        auto f2 = expr.top();
+        expr.pop();
+        bool operationValid = false;
+        int fnResult = caluculate(f1, f2, t[0], operationValid);
+        if (!operationValid)
+          return false;
+        expr.push(fnResult);
+      }
+    }
+    if (!expr.empty()) {
+      result = expr.top();
+      return true;
+    }
+    return false;
+  }
+
+private:
+  int caluculate(int a, int b, char op, bool &valid) {
+    valid = true;
+    if (op == '+')
+      return a + b;
+    if (op == '-')
+      return a - b;
+    if (op == '*')
+      return a * b;
+    if (op == '/') {
+      if (b == 0) {
+        valid = false;
         return 0;
-      expr.push(result);
+      }
+      float s = a / (float)b;
+      if (std::fmod(s, 1.0) == 0)
+        return (int)s;
+      valid = false;
+      return -1;
     }
-  }
-  return expr.top();
-}
-
-std::string converToInFix(const std::vector<const char *> &postfix) {
-  std::stack<std::string> expr;
-  for (auto &t : postfix) {
-    if (std::find(std::begin(operators), std::end(operators), *t) ==
-        std::end(operators)) {
-      expr.push(t);
-    } else {
-      auto f1 = expr.top();
-      expr.pop();
-      auto f2 = expr.top();
-      expr.pop();
-      expr.push("(" + f1 + t[0] + f2 + ")");
-    }
-  }
-  return expr.top();
-}
-
-void generatePostFix(std::vector<const char *> numbers, int stackHeight,
-                     std::vector<const char *> eq) {
-  if (found)
-    return;
-
-  if (stackHeight >= 2) {
-    for (const char &op : operators) {
-      auto temp = eq;
-      temp.push_back(&op);
-      generatePostFix(numbers, stackHeight - 1, temp);
-    }
+    return 0;
   }
 
-  bool allUsedUp = true;
-  for (int i = 0; i < numbers.size(); i++) {
-    if (numbers[i] != nullptr) {
-      allUsedUp = false;
-      const char *n = numbers[i];
-      numbers[i] = nullptr;
-      auto temp = eq;
-      temp.push_back(n);
-      generatePostFix(numbers, stackHeight + 1, temp);
-      numbers[i] = n;
-    }
-  }
-
-  if (allUsedUp && stackHeight == 1) {
-    int result = calculateFromPostfix(eq);
-    if (target == result) {
-      found = true;
-      resultString = converToInFix(eq);
-    }
-  }
-}
+private:
+  std::vector<const char *> data;
+};
 
 class NumberSearch : public INumberSearch {
 public:
-  void SetTargetNumber(const int value) override {
-    targetNumber = value;
-    target = value;
-  }
+  NumberSearch() : targetNumber(0) {}
 
-  void SetInputNumbers(const std::string &values) override {
-    std::istringstream iss(values);
+  void SetTargetNumber(const int value) override { targetNumber = value; }
+
+  void SetInputNumbers(const std::string &values) override { numbers = values; }
+
+  const std::string &GetSolution() override {
+    resultString.clear();
+
+    std::istringstream iss(numbers);
     std::string n;
+    std::vector<std::string> inputNumbers;
     while (iss >> n) {
       inputNumbers.push_back(n);
     }
-  }
 
-  const std::string &GetSolution() override {
     std::vector<const char *> numbers;
     for (auto &n : inputNumbers)
       numbers.push_back(n.c_str());
-    generatePostFix(numbers, 0, {});
+
+    testPostFixPosibilities(numbers, 0, {});
+
     return resultString;
   }
 
 private:
+  bool testPostFixPosibilities(std::vector<const char *> numbers, int stack,
+                                   PostFixEquation eq) {
+
+    int result = 0;
+    if (eq.calculateResult(result)) {
+      if (targetNumber == result) {
+        resultString = eq.toInFix();
+        return true;
+      }
+    }
+
+    if (stack >= 2) {
+      for (const char &op : operators) {
+        auto temp = eq;
+        temp.addItem(&op);
+        if (testPostFixPosibilities(numbers, stack - 1, temp))
+          return true;
+      }
+    }
+
+    for (int i = 0; i < numbers.size(); i++) {
+      if (numbers[i] != nullptr) {
+        const char *n = numbers[i];
+        numbers[i] = nullptr;
+        auto temp = eq;
+        temp.addItem(n);
+        if (testPostFixPosibilities(numbers, stack + 1, temp))
+          return true;
+        numbers[i] = n;
+      }
+    }
+    return false;
+  }
+
+private:
   int targetNumber;
-  std::vector<std::string> inputNumbers;
+  std::string numbers;
+  std::string resultString;
 };
 } // namespace aliiskenderturan
